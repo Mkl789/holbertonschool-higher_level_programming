1159856ac941051c91f58fa0e6781c3cfbb9bc53
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-09 11:14:27 +1200

    b

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,10 @@
 import sys 
 if __name__ == "__main__":
     for arg in sys.argv[1:]:
-        if arg == 0:
+        arg_count = len(sys.argv) - 1
+        if arg_count == 0:
             print("0 arguments.")
-        elif arg == 1:
+        elif arg_count == 1:
             print("1 argument: {}".format(arg))
-        elif arg > 1:
-            print("{} arguments: {}".format(arg, arg))
+        elif arg_count > 1:
+            print("{} arguments: {}".format(arg_count, arg))
