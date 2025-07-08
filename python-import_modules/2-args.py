3620c279ddf165954a31c710bdcbbc71a47f74e9
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-09 10:44:31 +1200

    g

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-
-    
+import sys 
+total = 0
+if __name__ == "__main__":
+    for arg in sys.argv[1:]:
+        total += int(arg)
+    if total == 1:
+        print("{} {}".format(total, "argument:"))
+    elif total > 1:
+        print("{} {}".format(total, "arguments:"))
+    else:
+        print("{} {}".format(total, "arguments."))
