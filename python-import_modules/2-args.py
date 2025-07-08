b2ae95445b254c95b814755bf589541b62656313
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-09 11:25:33 +1200

    ok

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys 
-    
+    import sys
+
     arg_count = len(sys.argv) - 1
     if arg_count == 0:
         print("0 arguments.")
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(arg_count))
-        
+
     for i in range(1, len(sys. argv)):
         print("{}: {}".format(i, sys.argv[i]))
