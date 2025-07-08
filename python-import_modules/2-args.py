3a38fcc20b3bc7bab08cf8c288a795db4d2f01c1
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-09 10:56:09 +1200

    tru

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,9 +4,11 @@ total = 0
 if __name__ == "__main__":
     for arg in sys.argv[1:]:
         total += int(arg)
-    if total == 1:
-        print("{} {}".format(total, "argument:"))
+    if total == 0:
+        print("{} {}".format(total, "arguments.", end=arg))
+    elif total == 1:
+        print("{} {}".format(total, "argument:", end=arg))
     elif total > 1:
-        print("{} {}".format(total, "arguments:"))
+        print("{} {}".format(total, "arguments:", end=arg))
     else:
-        print("{} {}".format(total, "arguments."))
+        pass
