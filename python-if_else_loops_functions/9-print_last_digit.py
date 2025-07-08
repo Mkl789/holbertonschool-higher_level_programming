45c38bfa92e09ce782c9dc9f13a376c5647097fe
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-08 13:13:22 +1200

    g

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+number = ""
+last_digit = abs(number) % 10
+last_digit = last_digit * -1
 def print_last_digit(number):
-    number = ""
-    last_digit = abs(number) % 10
     print(last_digit)
