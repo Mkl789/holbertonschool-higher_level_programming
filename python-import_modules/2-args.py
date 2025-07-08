7a1005d194a5b1a39535f732fc2994e4ff8f9729
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-09 11:10:21 +1200

    g

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
         if arg == 0:
             print("0 arguments.")
         elif arg == 1:
-            print("1 argument:", {})
+            print("1 argument: {}".format(arg))
         elif arg > 1:
             print("{} arguments: {}".format(arg, arg))
