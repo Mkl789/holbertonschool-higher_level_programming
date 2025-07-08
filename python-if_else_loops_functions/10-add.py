776c3e91068e0fefe1f19eef9cd578f3b9be689f
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-08 13:54:49 +1200

    ok

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def add(a, b):
-    if a < 0:
-        result = b - a
-    elif b < 0:
-        result = a - b
-    else:
-        result = a + b
-    print(result)
-    return result
+    return a + b
