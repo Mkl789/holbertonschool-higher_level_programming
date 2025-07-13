fc63b3813a910e495e4b057aafa088e2b1c6ef4d
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-13 19:20:50 +1200

    ah

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    print(sorted(a_dictionary))
+    sorted_dictionary = dict(sorted(a_dictionary.items()))
+    print(sorted_dictionary)
