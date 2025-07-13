65beb4fc866a60f03062edbf993d314496e6280d
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-13 19:42:07 +1200

    h

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    product = 2
-    for value in a_dictionary.values():
-        product *= value
-    return product 
+    new_dictionary = {}
+    for key in a_dictionary:
+        new_dictionary[key] = a_dictionary[key] * 2
+    return new_dictionary
