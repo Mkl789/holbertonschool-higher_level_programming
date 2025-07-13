93c35079516094d6774305b1681a03c16946fa39
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-13 13:06:06 +1200

    formating...

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,17 +4,22 @@ def add_tuple(tuple_a=(), tuple_b=()):
         first_ele_tuple_a = tuple_a[0]
     else:
         first_ele_tuple_a = 0
+
     if len(tuple_a) > 1:
         second_ele_tuple_a = tuple_a[1]
     else:
         second_ele_tuple_a = 0
+
     if len(tuple_b) > 0:
         first_ele_tuple_b = tuple_b[0]
     else:
         first_ele_tuple_b = 0
+
     if len(tuple_b) > 1:
         second_ele_tuple_b = tuple_b[1]
     else:
         second_ele_tuple_b = 0
-    result = first_ele_tuple_a + first_ele_tuple_b, second_ele_tuple_a + second_ele_tuple_b
+
+    result = (first_ele_tuple_a + first_ele_tuple_b,
+              second_ele_tuple_a + second_ele_tuple_b)
     return result
