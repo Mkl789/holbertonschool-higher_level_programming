ae7570d3d6ccd861fbcb20cb3808368b3bb467de
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-13 18:29:38 +1200

    h

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     old_element = search
     new_element = replace
     new_list = my_list
-    for i in range(len(my_list)):
-        if my_list[i] == search:
+    for i in range(len(new_list)):
+        if new_list[i] == search:
             new_list[i] = replace
     return new_list
