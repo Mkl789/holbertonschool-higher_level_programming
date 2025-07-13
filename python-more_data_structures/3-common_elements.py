4b6c5fb6f0a6c2ff22c8de9d9f6716fbeec550c3
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-13 19:03:52 +1200

    simple

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    seen = set()
-    duplicates = []
-    for element in set_1 and element in set_2:
-        if element in seen:
-            duplicates.append(element)
-        else:
-            seen.append(element)
-    return duplicates
+    common = set()
+    for element in set_1:
+        if element in set_2:
+            common.add(element)
+    return common
