0d8b51f638924b018a9a86dd87c5269cd46a8ca8
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-15 10:16:43 +1200

    ok

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string): 
-    result = ""
-    if not isinstance(roman_string, str) or roman_string == None:
-        return 0
-    for numeral in range(0x2160, 0x217):
-        result = chr(numeral)
-    return result
+def roman_to_int(s: str) -> int:
+    roman_map = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    total = 0
+
+    for i in range(len(s) - 1):
+        current_value = roman_map[s[i]]
+        next_value = roman_map[s[i+1]]
+
+        if current_value < next_value:
+            total -= current_value
+        else:
+            total += current_value
+
+    total += roman_map[s[-1]]
+
+    return total
